ef3319f8a50fc3ee46e8a44842620698a0dc2b68
SCadd148 <scadd14878@my.sunywcc.edu>
2025-03-14 12:31:59 -0400

    Add files via upload
    Improved UX

--- todo.py
+++ todo.py
@@ -1,6 +1,18 @@
+
+# save the newly inputted item
+# idea: pretty print the list (1 item on each line)
+# idea: clear the screen after we print the list
+# idea: delete item from the list
+# idea: edit an item in the list
+# idea: add numbers to the items when I print them out
+
+import os 
+def my_clear_func():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+my_clear_func()
 print("Welcome to my to-do list app!")
 my_list = []
-# save the newly inputted item
 while True:
     new_item = input("Please enter a new item or 'q' to quit:")
     if new_item == 'q':
@@ -8,4 +20,7 @@ while True:
         break
     else:
         my_list.append(new_item) # add new item to list
-        print(my_list)
+        my_clear_func()
+        print("My List:")
+        for item in my_list:
+            print("- " + item)
